a582924d248c45bb21a4375a5c0c0cb82f3c5360
Selim Aktay <sellimakty99@gmail.com>
2022-11-30 16:17:25 +0300

    Initial Commit

---
+++ BasicOBS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicOBS
+//
+//  Created by Selim on 30.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
